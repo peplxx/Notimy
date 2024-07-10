8476a100d7a25f6585235ee3bb3ac2e152daeab3
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-11 00:22:12 +0300

    feat: layout + styles from codepen

---
+++ notimy-frontend/src/assets/styles/variables.tsx
@@ -0,0 +1,29 @@
+export const htmlBackground = '#202123';
+export const header = '#202123';
+export const headerText = 'white';
+export const orderBody = '#afafaf';
+export const orderNotification = '#8d9afa';
+export const orderHeader = '#f9f9f7';
+export const messageBackground = '#333';
+export const messageText = '#fff';
+
+export const mainMaxWidth = '1000px';
+export const htmlMaxFontSize = '30px';
+
+
+// :root {
+//       /* colors */
+//     --html-background: #202123;
+//     --header: #202123;
+//     --header-text: white;
+//     --order__body: #afafaf;
+//     --order__notifications: #8d9afa;
+//     --order__header: #f9f9f7;
+//     --message-background: #333; /* Dark background for messages */
+//     --message-text: #fff; /* White text for messages */
+//
+//     /* sizes */
+//     --main-max-width: 1000px;
+//     --html-max-font-size: 30px;
+// }
+//
--- notimy-frontend/src/pages/index.tsx
+++ notimy-frontend/src/pages/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Header from '../components/layout/Header/Header';
 import Order from "../components/ui/Order/Order";
 import OrderList from "../components/layout/OrderList/OrderList";
-import styles from '../assets/styles/App.module.css'
-import gradients from './Order/Gradients.module.css'
+import styles from '../assets/styles/global.module.css'
+import gradients from '../assets/styles/Gradients.module.css'
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
         <div className={styles.Background}/>
         <Header title="Notimy" />
         <OrderList/>
-            {/*<Order title="BAZZAR" description="-Бургер Дмитрий" color={gradients.gradientPinkGreen} />*/}
-            {/*<Order title="108 BAR" description="-Медовуха Сергей" color={gradients.gradientYellowPurple} />*/}
-            {/*<Order title="InnoMAX" description="-Питса Никита" color={gradients.gradientYellowGreen} />*/}
+            <Order title="BAZZAR" description="-Бургер Дмитрий" color={gradients.gradientPinkGreen} />
+            <Order title="108 BAR" description="-Медовуха Сергей" color={gradients.gradientYellowPurple} />
+            <Order title="InnoMAX" description="-Питса Никита" color={gradients.gradientYellowGreen} />
     </div>
   );
 }
