35e964965d66a2a861aab0600cb1d91bfd6c8f45
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-10 18:40:48 +0300

    feat: add favicon + add headers

--- notimy-frontend/src/pages/_app.tsx
+++ notimy-frontend/src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import '../assets/styles/global.module.css';
 import 'normalize.css'
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Notimy</title>
+        <meta name="description" content="Notimy - в пизду пейджеры" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
