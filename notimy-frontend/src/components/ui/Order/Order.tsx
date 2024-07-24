ae1caaf75ad0920bf52737dcd6420b0694c15bf1
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-25 02:02:13 +0500

    feat: added green coloring

--- notimy-frontend/src/components/ui/Order/Order.tsx
+++ notimy-frontend/src/components/ui/Order/Order.tsx
@@ -10,11 +10,10 @@ import CryptoJS from 'crypto-js';
 
 interface Props {
     title: string;
-    description: string;
     id: string;
 }
 
-const Order: React.FC<Props> = ({ title, description, id }) => {
+const Order: React.FC<Props> = ({ title, id }) => {
     const [isOpen, setIsOpen] = useState(false);
     const toggleOrder = () => {
         setIsOpen(!isOpen);
@@ -23,9 +22,24 @@ const Order: React.FC<Props> = ({ title, description, id }) => {
     const hash: string = CryptoJS.MD5(id).toString(CryptoJS.enc.Hex);
     const hashNumber: number = parseInt(hash, 16) % 360;
 
-    const backgroundStyle = {
-        background: `linear-gradient(345deg, hsla(${hashNumber}, 71%, 79%, 1) 0%, hsla(${(hashNumber+100)%360}, 70%, 81%, 1) 60%);`
+
+    let mainHue1 = ((hashNumber) % 360);
+    if ( mainHue1 < 150 ) mainHue1 += mainHue1/150 * 50;
+    let mainHue2 = ((mainHue1 + 100) % 360);
+    let backgroundStyle = {
+            background: `linear-gradient(345deg, hsla(${mainHue2}, 71%, 79%, 1) 10%, hsla(${mainHue1}, 70%, 81%, 1) 60%)`
+        };
+
+    if ( title == 'BAZZAR' ) {
+        // Зеленый градиент (оттенки зелёного цвета)
+        const greenHue2 = (hashNumber % 40) + 100; // Диапазон 90-150
+        const greenHue1 = ((hashNumber + 30) % 60) + 90;
+
+        backgroundStyle = {
+            background: `linear-gradient(345deg, hsla(${greenHue2}, 70%, 85%, 1) 10%, hsla(${greenHue1}, 70%, 70%, 1) 60%)`
+        };
     }
+
 
     return (
         <div
