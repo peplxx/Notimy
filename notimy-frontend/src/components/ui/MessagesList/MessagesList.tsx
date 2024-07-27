74063a0463ea742e88d1d795ee3f39c50403824b
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-27 11:43:29 +0300

    feat: added input field as child ReactNode

--- notimy-frontend/src/components/ui/MessagesList/MessagesList.tsx
+++ notimy-frontend/src/components/ui/MessagesList/MessagesList.tsx
@@ -1,16 +1,21 @@
 import styles from "./MessagesList.module.css";
-import React from "react";
+import React, { ReactNode } from "react";
 
 
-const MessagesList: React.FC = () => {
+interface MessageListProps {
+    children: ReactNode;
+}
+
+const MessagesList: React.FC<MessageListProps> = ({children}) => {
     return (
         <div className={styles.messages}>
+            <div className={styles.message}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aliquam explicabo? Accusamus adipisci animi architecto beatae, corporis eaque error exercitationem illo libero nihil obcaecati quo reiciendis repellendus? Accusantium, ducimus itaque!</div>
             <div className={styles.message}>asd</div>
             <div className={styles.message}>asd</div>
             <div className={styles.message}>asd</div>
             <div className={styles.message}>asd</div>
             <div className={styles.message}>asd</div>
-            <div className={styles.message}>asd</div>
+            {children}
         </div>
     )
 }
