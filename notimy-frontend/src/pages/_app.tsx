a2ef070d739efa5936184f2193b3231e19c5c9be
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-27 14:05:51 +0300

    fix styles

--- notimy-frontend/src/pages/_app.tsx
+++ notimy-frontend/src/pages/_app.tsx
@@ -4,28 +4,31 @@ import Head from 'next/head';
 
 import Header from "../components/layout/Header/Header";
 
-
-import 'normalize.css'
-import '../assets/styles/global.css';
-import styles from '../assets/styles/global.module.css';
-
+// Import global styles
+import 'normalize.css';
+import '../assets/styles/global.css'; 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <>
-          <Head>
-              <title>Notimy</title>
-              <meta name="description" content="Notimy - в пизду пейджеры"/>
-              <meta name="viewport" content="width=device-width, initial-scale=1"/>
-              <link rel="icon" href="/logo.svg"/>
-              <link rel="preconnect" href="https://fonts.googleapis.com"/>
-              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-              <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
-                    rel="stylesheet"/>
-          </Head>
+    <>
+      <Head>
+        <title>Notimy</title>
+        <meta name="description" content="Notimy - в пизду пейджеры"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="/logo.svg"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
-          <Component {...pageProps} />;
-      </>
+      {/* You can include a global header if needed */}
+      <Header />
+
+      <Component {...pageProps} />
+    </>
   );
 }
 
