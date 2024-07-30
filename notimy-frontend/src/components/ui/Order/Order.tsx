cdbcb13bce37967deaa6ea0d7cfc12e55b51ecf2
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-31 00:30:39 +0300

    added funtcion for delete order

--- notimy-frontend/src/components/ui/Order/Order.tsx
+++ notimy-frontend/src/components/ui/Order/Order.tsx
@@ -1,25 +1,27 @@
-import React, {useState} from 'react';
+import React, { useRef, useState } from 'react';
 import OrderFooter from './Footer/OrderFooter';
 import OrderTop from '@/components/common/OrderTop/OrderTop';
 import styles from './Order.module.css';
 import classNames from 'classnames';
-
 import CryptoJS from 'crypto-js';
+import axiosInstance from '@/utils/axios';
 
-interface Props {
+interface OrderProps {
     title: string;
     id: string;
 }
 
-const Order: React.FC<Props> = ({title, id}) => {
+const Order: React.FC<OrderProps> = ({ title, id }) => {
+    const orderRef = useRef<HTMLDivElement>(null); // Убедитесь, что типизация правильная
     const [isOpen, setIsOpen] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+
     const toggleOrder = () => {
         setIsOpen(!isOpen);
     };
 
     const hash: string = CryptoJS.MD5(id).toString(CryptoJS.enc.Hex);
     const hashNumber: number = parseInt(hash, 16) % 360;
-
 
     let mainHue1 = ((hashNumber) % 360);
     if (mainHue1 < 150) mainHue1 += mainHue1 / 150 * 50;
@@ -28,8 +30,7 @@ const Order: React.FC<Props> = ({title, id}) => {
         background: `linear-gradient(345deg, hsla(${mainHue2}, 71%, 79%, 1) 10%, hsla(${mainHue1}, 70%, 81%, 1) 60%)`
     };
 
-    if (title == 'BAZZAR') {
-        // Зеленый градиент (оттенки зелёного цвета)
+    if (title === 'BAZZAR') { // Используйте строгое равенство для сравнения строк
         const greenHue2 = (hashNumber % 40) + 100; // Диапазон 90-150
         const greenHue1 = ((hashNumber + 30) % 60) + 90;
 
@@ -38,17 +39,35 @@ const Order: React.FC<Props> = ({title, id}) => {
         };
     }
 
+    async function deleteChannel() {
+        return axiosInstance.delete(`api/channel/${id}`).then(async (response) => {
+            if (response.status === 200) {
+                setIsDeleting(true);
+                await new Promise(r => setTimeout(r, 500));
+                orderRef.current.remove();
+                return true;
+            }
+            return false;
+        }).catch(async (e) => {
+            return false;
+        });
+    };
 
     return (
         <div
+            ref={orderRef}
             className={classNames(styles.order, {
                 [styles.orderClosed]: !isOpen,
                 [styles.orderOpened]: isOpen,
-            })}
+            },
+                {
+                    [styles.orderSlidingOut]: isDeleting
+                }
+            )}
             onClick={toggleOrder}
         >
-            <OrderTop title={title} backgroundStyle={backgroundStyle}/>
-            <OrderFooter backgroundStyle={backgroundStyle}/>
+            <OrderTop title={title} backgroundStyle={backgroundStyle} DeleteAction={deleteChannel} />
+            <OrderFooter backgroundStyle={backgroundStyle} />
         </div>
     );
 };
