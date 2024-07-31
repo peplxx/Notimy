491baaf8eec3b545df37a57ee4a0fa2b961fd9a7
dmitrii <prosvirkin.dmitry@gmail.com>
2024-07-31 21:10:47 +0300

    added sending cookies

--- notimy-frontend/src/utils/axios.tsx
+++ notimy-frontend/src/utils/axios.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://0.0.0.0:5000"
+    baseURL: "http://0.0.0.0:5000",
+    withCredentials: true,
 });
 
 export default axiosInstance;
