129beaa65289e1ec9f37b6b63d4da1f674a2fa47
dmitrii <prosvirkin.dmitriy@yandex.ru>
2024-09-12 13:44:59 +0300

    feat: add landing

--- frontend/src/pages/landing.js
+++ frontend/src/pages/landing.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, {useEffect, useRef} from 'react';
+import {Link, useNavigate} from "react-router-dom";
+
+import styles from './landing.module.css';
+import Slider from "../components/Slider";
 
 const Landing = () => {
+    const parent = useRef(null);
+    const navigate = useNavigate();
+
+    const handleSliderDone = () => {
+        navigate('/app'); // Navigate to the '/app' route
+    };
+
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            color: "white",
-            textAlign: "center",
-            margin: "auto",
-        }}>
-            <p>Скоро здесь появится информация<br /> Рабочее приложение - <Link to={'/app'} style={{ color: 'lightblue' }}>перейти</Link></p>
-        </div>
+        <>
+            <img className={styles.presentation} src='/notimy_presentation.jpg' alt="Presentation"/>
+            <div className={styles.main} ref={parent}>
+                <Slider parentRef={parent} sliderColor={'rgb(130,255,27)'} onDone={handleSliderDone} />
+                <span className={styles.btn_text}>Открыть приложение</span>
+            </div>
+        </>
     );
 };
 
