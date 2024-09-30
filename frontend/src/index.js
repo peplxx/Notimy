84875d3b045dba4c6a302a0f78d8145962fef7fa
dmitrii <prosvirkin.dmitriy@yandex.ru>
2024-09-30 23:25:41 +0300

    feat: toaster

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 
 import 'normalize.css';
 import './index.css';
-import {sendSubscriptionToServer} from "./utils/api";
-import sleep from "./utils/sleep";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,47 +13,3 @@ root.render(
         <App/>
     </React.StrictMode>
 );
-// Публичный VAPID ключ (замените на ваш ключ)
-const vapidPublicKey = 'BFvjzC0mYeMAWz9rJCl1EuNSsWpK_1ZEk_JjFzb_VSjAx9_cMtexEBm5pCcMz2mo2IqAqQ0JmlBoyckdpkTDO10';
-const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
-
-// Функция для конвертации ключа VAPID
-function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-    const base64 = (base64String + padding)
-        .replace(/-/g, '+')
-        .replace(/_/g, '/');
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-}
-
-window.addEventListener('load', async () => {
-    const register = await navigator.serviceWorker.register('/sw.js', {
-        scope: '/'
-    });
-
-    // Подписываемся на push уведомления
-    const subscription = await register.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey)
-    });
-
-    // Если подписка существует, отправляем её на сервер для проверки
-    sendSubscriptionToServer(subscription);
-
-    await sleep(1000);
-
-    Notification.requestPermission().then(permission => {
-        if (permission === 'granted') {
-            // Теперь можно регистрировать push-уведомления
-        } else {
-            console.log('Разрешение на уведомления не предоставлено.');
-        }
-    });
-});
--- frontend/src/pages/Index.js
+++ frontend/src/pages/Index.js
@@ -1,15 +1,78 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import OrderList from 'components/User/OrderList';
 import {UserProvider} from "context/UserContext";
 import Header from "components/Header/Header";
 import ScanBtn from "../components/User/ScanBtn";
+import {Toaster, toast} from "sonner";
+import {sendSubscriptionToServer} from "../utils/api";
+
+// Публичный VAPID ключ (замените на ваш ключ)
+const vapidPublicKey = 'BFvjzC0mYeMAWz9rJCl1EuNSsWpK_1ZEk_JjFzb_VSjAx9_cMtexEBm5pCcMz2mo2IqAqQ0JmlBoyckdpkTDO10';
+const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+
+// Функция для конвертации ключа VAPID
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
 const Home = () => {
+    useEffect(() => {
+        const registerServiceWorkerAndSubscribe = async () => {
+            try {
+                // Регистрация service worker
+                const register = await navigator.serviceWorker.register('/sw.js', {
+                    scope: '/'
+                });
+
+                // Подписываемся на push уведомления
+                const subscription = await register.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: convertedVapidKey
+                });
+                console.log(subscription);
+                toast.success('Подписка выполнена');
+
+                // Отправляем подписку на сервер
+                await sendSubscriptionToServer(subscription);
+            } catch (e) {
+                toast.error(`Ошибка регистрации или подписки: ${e.message}`);
+            }
+
+            // Запрашиваем разрешение на уведомления
+            try {
+                const permission = await Notification.requestPermission();
+                if (permission === 'granted') {
+                    toast.success('Разрешение на уведомления предоставлено');
+                } else {
+                    toast.error('Разрешение на уведомления не предоставлено');
+                }
+            } catch (e) {
+                toast.error(`Ошибка запроса разрешения на уведомления: ${e.message}`);
+            }
+        };
+
+        // Запускаем асинхронную функцию
+        registerServiceWorkerAndSubscribe();
+    }, []); // Пустой массив зависимостей для выполнения только при монтировании компонента
+
     return (
         <div>
-            <Header/>
+            <Toaster />
+            <Header />
             <UserProvider>
                 <OrderList>
-                    <ScanBtn/>
+                    <ScanBtn />
                 </OrderList>
             </UserProvider>
         </div>
