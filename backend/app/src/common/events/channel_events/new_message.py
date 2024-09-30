ceb6c7f4d81c3d9960b95ef30ece88adf81d1794
peplxx <seregaopkymelnikov@yandex.ru>
2024-09-30 03:18:52 +0300

    feat: Use config in new message event

--- backend/app/src/common/events/channel_events/new_message.py
+++ backend/app/src/common/events/channel_events/new_message.py
@@ -1,8 +1,9 @@
 __all__ = ["NewMessageEvent"]
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.data.db.models import Channel, Message
+from app.data.db.models import Channel, Message, Provider
 from app.src.common.events.channel_events.base import ChannelEvent
+from app.src.common.push_notifications import DefaultPushNotification
 from app.src.common.push_notifications.dto import PushNotification
 from app.src.modules.spots.logic import add_message
 
@@ -11,10 +12,10 @@ event_type = "NewMessageEvent"
 
 class NewMessageEvent(ChannelEvent):
     event_type: str = ChannelEvent._event_type(event_type)
-    pushNotification: PushNotification = PushNotification(title=f"У вас новое сообщение!", body="какой то текст")
 
     def __init__(self, message: Message, source: Channel, session: AsyncSession) -> None:
         self.message = message
+        self.pushNotification = DefaultPushNotification(title=f"У вас новое сообщение!", body=f"{self.message.text}")
         super().__init__(source, session)
 
     async def invoke(self):
