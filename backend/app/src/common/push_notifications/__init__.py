82a70f0c1ea6d57276c694ab9147c0a38598e4e2
peplxx <seregaopkymelnikov@yandex.ru>
2024-10-07 06:00:09 +0300

    fix: add telegram push

--- backend/app/src/common/push_notifications/__init__.py
+++ backend/app/src/common/push_notifications/__init__.py
@@ -1,3 +1,7 @@
 __all__ = ["DefaultPushNotification"]
 
 from app.src.common.push_notifications.default import DefaultPushNotification
+from app.src.common.push_notifications.dto import PushNotification
+from app.src.common.push_notifications.telegram_api.send_message import send_telegram_message
+
+
