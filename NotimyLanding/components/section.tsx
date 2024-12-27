2bc73829732e831483f9e195d4c18f6927f639ab
dmitrii <prosvirkin.dmitriy@yandex.ru>
2024-12-27 18:10:16 +0500

    add sections

---
+++ NotimyLanding/components/section.tsx
@@ -0,0 +1,11 @@
+export const Section = () => {
+  return (
+    <section>
+      <div className="w-full h-[36rem] my-10 p-10 rounded-3xl bg-slate-800">
+        <div className="h-full ">
+          asd
+        </div>
+      </div>
+    </section>
+  )
+}
