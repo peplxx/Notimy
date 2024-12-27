a5ff73e11f0f46ff31410f4e9f796023870b590d
dmitrii <prosvirkin.dmitriy@yandex.ru>
2024-12-27 18:21:28 +0500

    add button tg

--- NotimyLanding/components/section.tsx
+++ NotimyLanding/components/section.tsx
@@ -1,7 +1,7 @@
 export const Section = () => {
   return (
     <section>
-      <div className="w-full h-[36rem] my-10 p-10 rounded-3xl bg-slate-800">
+      <div className="w-full h-[36rem] my-10 p-10 rounded-3xl bg-slate-100">
         <div className="h-full ">
           asd
         </div>
